5c7d8a10a42a6706e60eace3fa93432c7cd81650
Bernardo Viseu <bernardo2viseu@gmail.com>
2018-01-13 00:30:34 +0000

    fixes tabs and spaces problem (praise vim :retab)

--- 50QuestoesP12016.c
+++ 50QuestoesP12016.c
@@ -24,342 +24,342 @@ int main()
 void menu(){
     int a;
     while(a!=0){
-    	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nPROBLEMA A RESOLVER (0=Exit):\nQuestão nº:");
-	    scanf("%d",&a);
-    	fflush(stdin);
-	    switch (a){//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!TODO: COMPLETAR OS QUE ESTAO EM COMENTARIO
-	        case 1: q1(); break;
+        printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nPROBLEMA A RESOLVER (0=Exit):\nQuestão nº:");
+        scanf("%d",&a);
+        fflush(stdin);
+        switch (a){//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!TODO: COMPLETAR OS QUE ESTAO EM COMENTARIO
+            case 1: q1(); break;
             case 2: q2(); break;
-		    case 3: q3(); break;
-		    case 4: q4(); break;
-		    case 5: q5(); break;
-		    case 6: q6(); break;
-		    case 7: q7(); break;
-		    case 8: q8(); break;
-		    case 9: q9(); break;
-		    //case 10: q10(); break;
-		    case 11: q11(); break;
-		    //case 12: q12(); break;
-		    case 13: q13(); break;
-		    case 14: q14(); break;
-		    //case 15: q15(); break;
-		    //case 16: q16(); break;
-		    case 17: q17(); break;
-		    case 18: q18(); break;
-		    //case 19: q19(); break;
-		    //case 20: q20(); break;
-		    //case 21: q21(); break;
-		    case 22: q22(); break;
-		    case 23: q23(); break;
-		    //case 24: q24(); break;
-	        default: printf("NOT VALID\n"); break;
-	    }
+            case 3: q3(); break;
+            case 4: q4(); break;
+            case 5: q5(); break;
+            case 6: q6(); break;
+            case 7: q7(); break;
+            case 8: q8(); break;
+            case 9: q9(); break;
+            //case 10: q10(); break;
+            case 11: q11(); break;
+            //case 12: q12(); break;
+            case 13: q13(); break;
+            case 14: q14(); break;
+            //case 15: q15(); break;
+            //case 16: q16(); break;
+            case 17: q17(); break;
+            case 18: q18(); break;
+            //case 19: q19(); break;
+            //case 20: q20(); break;
+            //case 21: q21(); break;
+            case 22: q22(); break;
+            case 23: q23(); break;
+            //case 24: q24(); break;
+            default: printf("NOT VALID\n"); break;
+        }
     }
 }
  
 void q1()
 {
-	printf("Insere numeros que queres somar (0 para terminar)\n");
-	int a,n = 0;
-	scanf("%d", &a);
+    printf("Insere numeros que queres somar (0 para terminar)\n");
+    int a,n = 0;
+    scanf("%d", &a);
     while(a!=0){
-    	n+=a;
-    	scanf("%d", &a);
+        n+=a;
+        scanf("%d", &a);
     }
     printf("Soma: %d\n", n);
 }
  
 void q2()
 {
-	printf("Insere numeros (0 para mostrar o maior deles)\n");
-	int a,n = 0;
-	scanf("%d", &a);
-	n = a;
-	 while(a!=0){
-    	if (n < a){n = a;}
-    	scanf("%d", &a);
+    printf("Insere numeros (0 para mostrar o maior deles)\n");
+    int a,n = 0;
+    scanf("%d", &a);
+    n = a;
+     while(a!=0){
+        if (n < a){n = a;}
+        scanf("%d", &a);
     }
     printf("Maior número: %d\n", n);
 }
 
 void q3()
 {
-	printf("Insere numeros (0 para mostrar a media)\n");
-	int a,b = 0 ,i = 0,m;
-	scanf("%d", &a);
-	while(a!=0){
-		b += a;
-		scanf("%d", &a);
-		++i;
-	}
-	m = b/i;
-	printf("Media: %d\n", m);
+    printf("Insere numeros (0 para mostrar a media)\n");
+    int a,b = 0 ,i = 0,m;
+    scanf("%d", &a);
+    while(a!=0){
+        b += a;
+        scanf("%d", &a);
+        ++i;
+    }
+    m = b/i;
+    printf("Media: %d\n", m);
 }
 
 void q4()
 {
-	printf("Insere numeros (0 para mostrar o segundo maior numero)\n");
-	int a, b, c;
-	scanf("%d", &a);
-	b = a;
-	c = a;
-	while(a!=0){
-		if (a>b) {c = b; b = a;} else if (a>c){c=a;}
-		scanf("%d", &a);
-	}
-	printf("Segundo maior numero: %d\n", c);
+    printf("Insere numeros (0 para mostrar o segundo maior numero)\n");
+    int a, b, c;
+    scanf("%d", &a);
+    b = a;
+    c = a;
+    while(a!=0){
+        if (a>b) {c = b; b = a;} else if (a>c){c=a;}
+        scanf("%d", &a);
+    }
+    printf("Segundo maior numero: %d\n", c);
 }
 
 void q5()
 {
-	int n;
-	printf("Insere um numero positivo para contar a quantidade de 1's na representaçao binaria.\n");
-	scanf("%d",&n);
-	printf("Numero de 1's na representaçao binaria: %d.\n", bitsUm(n));
+    int n;
+    printf("Insere um numero positivo para contar a quantidade de 1's na representaçao binaria.\n");
+    scanf("%d",&n);
+    printf("Numero de 1's na representaçao binaria: %d.\n", bitsUm(n));
 }
 
 int bitsUm(unsigned int n)
 {
-	int r=0;
-	while(n!=0){
-		r+=(n%2);
-		n=(n/2);
-	}
-	return r;
+    int r=0;
+    while(n!=0){
+        r+=(n%2);
+        n=(n/2);
+    }
+    return r;
 }
 
 void q6()
 {
-	int n;
-	printf("Insere um numero positivo para contar a quantidade de 0's na representaçao binaria.\n");
-	scanf("%d",&n);
-	printf("Numero de 0's na representaçao binaria: %d\n", trailingZ(n)); 
+    int n;
+    printf("Insere um numero positivo para contar a quantidade de 0's na representaçao binaria.\n");
+    scanf("%d",&n);
+    printf("Numero de 0's na representaçao binaria: %d\n", trailingZ(n)); 
 }
 
 int trailingZ(unsigned int n)
 {
-	int r=0;
-	while(n!=0){
-		if (n%2==0){r++;}
-		n=n/2;
-	}
-	return r;
+    int r=0;
+    while(n!=0){
+        if (n%2==0){r++;}
+        n=n/2;
+    }
+    return r;
 }
 
 void q7()
 {
-	int n;
-	printf("Insere um numero para contar os digitos.\n");
-	scanf("%d",&n);
-	printf("Numero de digitos: %d\n", qDig(n));
+    int n;
+    printf("Insere um numero para contar os digitos.\n");
+    scanf("%d",&n);
+    printf("Numero de digitos: %d\n", qDig(n));
 }
 
 int qDig(unsigned int n)
 {
-	int r=0;
-	while(n!=0){
-		n/=10;
-		r++;
-	}
-	return r;
+    int r=0;
+    while(n!=0){
+        n/=10;
+        r++;
+    }
+    return r;
 }
 
 void q8()
 {
-	char n[256];
-	printf("Escreve uma string, recebe o comprimento dela.\n");
-	scanf("%s",&n);
-	printf("Comprimento da string: %d\n", strLength(n));
+    char n[256];
+    printf("Escreve uma string, recebe o comprimento dela.\n");
+    scanf("%s",&n);
+    printf("Comprimento da string: %d\n", strLength(n));
 }
 
 int strLength(char str[])
 {
-	int r=0;
-	while(str[r]!='\0'){
-		r++;
-	}
-	return r;
+    int r=0;
+    while(str[r]!='\0'){
+        r++;
+    }
+    return r;
 }
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!TODO : PERCEBER ESTE!!
 void q9()
 {
-	char s1[256], s2[256];
-	printf("Escreve duas strings para as juntar.\nString 1:");
-	scanf("%s",&s1);
-	printf("String 2:");
-	scanf("%s",&s2);
-	strCat(s1, s2);
-	printf("Resultado: %s\n %d\n", s1, strlen(s1));
+    char s1[256], s2[256];
+    printf("Escreve duas strings para as juntar.\nString 1:");
+    scanf("%s",&s1);
+    printf("String 2:");
+    scanf("%s",&s2);
+    strCat(s1, s2);
+    printf("Resultado: %s\n %d\n", s1, strlen(s1));
 }
 
 char *strCat(char s1[], char s2[])
 {
-	//char *res = (char *) malloc(strlen(s1)+strlen(s2)+1);
-	int i;
-	s1[(strlen(s1))] = malloc(sizeof(s1)+sizeof(s2));
-	for (i = 0; s2[i]!='\0'; i++)
-		s1[(strlen(s1)+i)] = s2[i];
-	return s1;
+    //char *res = (char *) malloc(strlen(s1)+strlen(s2)+1);
+    int i;
+    s1[(strlen(s1))] = malloc(sizeof(s1)+sizeof(s2));
+    for (i = 0; s2[i]!='\0'; i++)
+        s1[(strlen(s1)+i)] = s2[i];
+    return s1;
 }
 
 void q11()
 {
-	char s1[256], s2[256];
-	printf("Insere duas strings para comparar:\nString 1:");
-	scanf("%s",&s1);
-	printf("String 2:");
-	scanf("%s",&s2);
-	printf("Resultado: ");
-	if(strcmp1(s1, s2)>0) printf("String 1 > String 2\n");
-	if(strcmp1(s1, s2)==0) printf("String 1 = String 2\n");
-	if(strcmp1(s1, s2)<0) printf("String 1 < String 2\n");
+    char s1[256], s2[256];
+    printf("Insere duas strings para comparar:\nString 1:");
+    scanf("%s",&s1);
+    printf("String 2:");
+    scanf("%s",&s2);
+    printf("Resultado: ");
+    if(strcmp1(s1, s2)>0) printf("String 1 > String 2\n");
+    if(strcmp1(s1, s2)==0) printf("String 1 = String 2\n");
+    if(strcmp1(s1, s2)<0) printf("String 1 < String 2\n");
 }
 
 int strcmp1(char s1[], char s2[])
 {
-	int r=0,i;
-	for (i = 0;r==0 && s1[i]!='\0' && s2[i]!='\0'; i++)
-	{
-		r += s1[i]-s2[i];
-	}
-	return r;
+    int r=0,i;
+    for (i = 0;r==0 && s1[i]!='\0' && s2[i]!='\0'; i++)
+    {
+        r += s1[i]-s2[i];
+    }
+    return r;
 }
 /*
 char *strstr(char s1[], char s2[])
 {
-	int i, j=0;
-	for (i = 0;s1[i]!='\0' && s2[j]!='\0'; i++)
-	{
-		if(s1[i]==s2[j]) j++;
-		else j=0;
-	}
-	if(j==0) return NULL;
-	return s1 + i;
+    int i, j=0;
+    for (i = 0;s1[i]!='\0' && s2[j]!='\0'; i++)
+    {
+        if(s1[i]==s2[j]) j++;
+        else j=0;
+    }
+    if(j==0) return NULL;
+    return s1 + i;
 }
 */
 void q13()
 {
-	char s1[256];
-	printf("Insere string para dares reverse:\n");
-	scanf("%s",&s1);
-	strREV(s1);
+    char s1[256];
+    printf("Insere string para dares reverse:\n");
+    scanf("%s",&s1);
+    strREV(s1);
 }
 
 void strREV(char s[])
 {
-	int	i;
-	char s2[strlen(s)];
-	strcpy(s2,s);
-	for (i = 0; s[i]!='\0'; i++)
-	{
-		s[i]= s2[(strlen(s)-i-1)];
-	}
-	printf("Toma: %s\n", s);
+    int i;
+    char s2[strlen(s)];
+    strcpy(s2,s);
+    for (i = 0; s[i]!='\0'; i++)
+    {
+        s[i]= s2[(strlen(s)-i-1)];
+    }
+    printf("Toma: %s\n", s);
 }
 
 void q14()
 {
-	char s1[256];
-	printf("Insere string para tirares vogais:\n");
-	scanf("%s",&s1);
-	strNOV(s1);
+    char s1[256];
+    printf("Insere string para tirares vogais:\n");
+    scanf("%s",&s1);
+    strNOV(s1);
 }
 
 void strNOV(char s[])
 {
-	int i,j=0;
-	for (i = 0; s[i]!='\0'; i++)
-	{
-		if (s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U');
-		else {s[j] = s[i]; j++;}
-	}
-	s[j]='\0';
-	printf("Toma: %s\n", s);
+    int i,j=0;
+    for (i = 0; s[i]!='\0'; i++)
+    {
+        if (s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U');
+        else {s[j] = s[i]; j++;}
+    }
+    s[j]='\0';
+    printf("Toma: %s\n", s);
 }
 
 void q17()
 {
-	char s1[256];
-	printf("Escreve string para contar maximo de iguais consecutivos:\n");
-	scanf("%s",&s1);
-	iguaisConsecutivos(s1);
+    char s1[256];
+    printf("Escreve string para contar maximo de iguais consecutivos:\n");
+    scanf("%s",&s1);
+    iguaisConsecutivos(s1);
 }
 
 void iguaisConsecutivos(char s[])
 {
-	int i, j, r=0, rr=0;
-	for (i=0;s[i]!='\0';i++)
-	{
-		for (j=i;s[j]!='\0';j++)
-		{
-			if (s[j+1]==s[j] && s[j+1]!='\0') rr++;
-			else break;
-		}
-		rr++;
-		if (rr>r) r=rr; rr=0;
-	}
-	printf("Toma: %d\n", r);
+    int i, j, r=0, rr=0;
+    for (i=0;s[i]!='\0';i++)
+    {
+        for (j=i;s[j]!='\0';j++)
+        {
+            if (s[j+1]==s[j] && s[j+1]!='\0') rr++;
+            else break;
+        }
+        rr++;
+        if (rr>r) r=rr; rr=0;
+    }
+    printf("Toma: %d\n", r);
 }
 
 void q18()
 {
-	char s1[256];
-	printf("Escreve string para contar maximo de diferentes consecutivos:\n");
-	scanf("%s",&s1);
-	diffConsecutivos(s1);
+    char s1[256];
+    printf("Escreve string para contar maximo de diferentes consecutivos:\n");
+    scanf("%s",&s1);
+    diffConsecutivos(s1);
 }
 
 void diffConsecutivos(char s[])
 {
-	int i, j, r=0, rr=0;
-	for (i=0;s[i]!='\0';i++)
-	{
-		for (j=i;s[j]!='\0';j++)
-		{
-			if (s[j+1]!=s[j] && s[j+1]!='\0') rr++;
-			else break;
-		}
-		rr++;
-		if (rr>r) r=rr; rr=0;
-	}
-	printf("Toma: %d\n", r);
+    int i, j, r=0, rr=0;
+    for (i=0;s[i]!='\0';i++)
+    {
+        for (j=i;s[j]!='\0';j++)
+        {
+            if (s[j+1]!=s[j] && s[j+1]!='\0') rr++;
+            else break;
+        }
+        rr++;
+        if (rr>r) r=rr; rr=0;
+    }
+    printf("Toma: %d\n", r);
 }
 
 void q22()
 {
-	char s1[100];
-	printf("Escreve uma string para contar as palavras:\n");
-	scanf(" %[^\n]s",&s1);
-	contaPal(s1);
+    char s1[100];
+    printf("Escreve uma string para contar as palavras:\n");
+    scanf(" %[^\n]s",&s1);
+    contaPal(s1);
 }
 
 void contaPal(char s[])
 {
-	int i, conta=0;
-	for (i=0; s[i]!='\0'; i++)
-	{
-		if (s[i]!=' ' && s[i+1]==' ' || s[i+1]=='\0') conta++;
-	}
-	printf("Numero de palavras: %d\n", conta);
+    int i, conta=0;
+    for (i=0; s[i]!='\0'; i++)
+    {
+        if (s[i]!=' ' && s[i+1]==' ' || s[i+1]=='\0') conta++;
+    }
+    printf("Numero de palavras: %d\n", conta);
 }
 
 void q23()
 {
-	char s1[256];
-	printf("Escreve uma string para contar as vogais:\n");
-	scanf("%s",&s1);
-	printf("Numero de vogais: %d\n", contaVogais(s1));
+    char s1[256];
+    printf("Escreve uma string para contar as vogais:\n");
+    scanf("%s",&s1);
+    printf("Numero de vogais: %d\n", contaVogais(s1));
 }
 
 int contaVogais(char s[])
 {
-	int i, res=0;
-	for (i=0;s[i]!='\0';i++)
-	{
-		if (s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
-			res++;
-	}
-	return res;
+    int i, res=0;
+    for (i=0;s[i]!='\0';i++)
+    {
+        if (s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
+            res++;
+    }
+    return res;
 }
-	
+    
