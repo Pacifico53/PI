4b89e4d28fc11406550b4fedc09a67f4df269cee
Pacifico53 <bernardo2viseu@gmail.com>
2018-02-21 14:02:33 +0000

    melhora a ficha2

--- Ficha2/ficha2_1718.c
+++ Ficha2/ficha2_1718.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+void parte1(); void parte2();
 void f1(); float mult(int n, float m); float mult2(int n, float m); int mdc(int a, int b); int fibrec(int n); int fib(int n);
 void f2();
+void f3();
+void f2_1(int a, int b); void f2_2(int a, int b);
 
 int main()
 {
+    int p=0;
+    printf("ESCOLHE PARTE (1 OU 2):\n");
+    while (p<1 || p>2) {
+        scanf("%d", &p);
+        if (p==1) {
+            parte1();
+        } else parte2();
+    }
+    /*
     f1();
     printf("%f\n", mult(10, 4));
     printf("%f\n", mult2(5, 6));
@@ -13,10 +26,52 @@ int main()
     printf("%d\n", fibrec(10));
     printf("%d\n", fib(10));
     f2();
+    */
     return 0;
 }
 
-//------------PARTE 1--------------
+void parte1(){
+    int q=0;
+    while (q<1 || q>6) {
+        printf("ESCOLHE QUESTAO:\n");
+        scanf("%d", &q);
+        switch (q) {
+            case 1: f1();
+                    break;
+            case 2: f2();
+                    break;
+            case 3: f3();
+                    break;
+            default: printf("De 1 a 5 amigo ;)\n");
+        }
+    }
+}
+
+void parte2(){
+    int q=0;
+    srand(time(NULL));
+    while (q<1 || q>6) {
+        printf("ESCOLHE QUESTAO:\n");
+        scanf("%d", &q);
+        switch (q) {
+            case 1: f2_1(rand()%15, rand()%15);
+                    break;
+            case 2: f2_2(rand()%15, rand()%15);
+                    break;
+            case 3: f3();
+                    break;
+            case 4: f1();
+                    break;
+            case 5: f1();
+                    break;
+            case 6: f1();
+                    break;
+            default: printf("De 1 a 5 amigo ;)\n");
+        }
+    }
+}
+
+//-------------------PARTE 1---------------
 
 void f1(){
     int i, j, *a, *b;
@@ -44,13 +99,19 @@ void f2(){
     initCOOL(&x);
     printf("%d\n", x);
 }
-//TODO
-/*
-void swap(int a, int b){
+
+void swap(int *a, int *b){
     int x;
-    x = a; a = b; b = a;
+    x = *a; *a = *b; *b = x;
 }
-*/
+
+void f3(){
+    int x=3, y=5;
+    swap(&x, &y);
+    printf("%d %d\n", x, y);
+}
+
+
 
 //---------------PARTE 2--------------
 
@@ -63,6 +124,11 @@ float mult(int n, float m){
     return r;
 }
 
+void f2_1(int a, int b){
+    int r = mult(a, b);
+    printf("OUTPUT PARA A = %d, E B = %d = %d\n", a, b, r);   
+}
+
 float mult2(int n, float m){
     float r = 0;
     while (n>0) {
@@ -71,6 +137,11 @@ float mult2(int n, float m){
         m += m;
     }
     return r;
+}
+
+void f2_2(int a, int b){
+    int r = mult2(a, b);
+    printf("OUTPUT PARA A = %d, E B = %d = %d\n", a, b, r);   
 }
 
 int mdc(int a, int b){
